c71cfdbb4ada40554b760b13f0ef22a49eae61c8
Nick Szydlowski <nick.szydlowski@sjsu.edu>
2024-09-07 19:59:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,8 +84,12 @@ if st.session_state.messages[-1]["role"] != "assistant":
             except IndexError:
                 st.error("We hit a bump - let's try again")
                 st.write("test1")
-                resp = st.session_state.chat_engine.chat(prompt)
-                st.write("test2")
+                try:
+                    st.write("test2")
+                    resp = st.session_state.chat_engine.chat(prompt)
+                    st.write(resp)
+                except StopCandidateException:
+                        st.error("We got an error from Google Gemini - usually this means the question had a risk of producing a harmful response. Try asking the question a different way.")
         message = {"role": "assistant", "content": response_stream.response}
         # Add response to message history
         st.session_state.messages.append(message)
