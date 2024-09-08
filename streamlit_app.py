fa41efa06a3d575f2e5a7bce1f2d5066c9f49401
Nick Szydlowski <nick.szydlowski@sjsu.edu>
2024-09-07 20:42:23 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,16 +82,17 @@ if st.session_state.messages[-1]["role"] != "assistant":
             response_stream = st.session_state.chat_engine.stream_chat(prompt)
         except:
             st.error("We got an error from Google Gemini - this may mean the question had a risk of producing a harmful response. Consider asking the question in a different way.")        
-        with st.spinner("waiting"):
-            try:
-                st.write_stream(response_stream.response_gen)
-            except:
-                st.error("We hit a bump - let's try again")
+        if response_stream != ""
+            with st.spinner("waiting"):
                 try:
-                    resp = st.session_state.chat_engine.chat(prompt)[0]
-                    st.write(resp)
+                    st.write_stream(response_stream.response_gen)
                 except:
-                    st.error("We got an error from Google Gemini - this may mean the question had a risk of producing a harmful response. Consider asking the question in a different way.")
+                    st.error("We hit a bump - let's try again")
+                    try:
+                        resp = st.session_state.chat_engine.chat(prompt)[0]
+                        st.write(resp)
+                    except:
+                        st.error("We got an error from Google Gemini - this may mean the question had a risk of producing a harmful response. Consider asking the question in a different way.")
         message = {"role": "assistant", "content": response_stream.response}
         # Add response to message history
         st.session_state.messages.append(message)
