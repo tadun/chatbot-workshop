86dcfd58f86801d8a8ac388d8625b0b6580bc94d
Nick Szydlowski <nick.szydlowski@sjsu.edu>
2024-09-19 11:47:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,15 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.embeddings.gemini import GeminiEmbedding
 
 
-st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Chat with the works of Rabindranath Tagore, powered by LlamaIndex 💬🦙")
+st.set_page_config(page_title="Chat with an export on the works of Rabindranath Tagore", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("Chat with an export on the works of Rabindranath Tagore")
 st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Ask me a question about the Unionist!",
+            "content": "Ask me a question about Rabindranath Tagore!",
         }
     ]
 
