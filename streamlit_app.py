12a6e07dca758cfd8a511982593f79f428f3e302
Nick Szydlowski <nick.szydlowski@sjsu.edu>
2024-09-19 11:26:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ def load_data():
         model="models/gemini-1.5-flash",
         temperature=0.2,
         system_prompt="""You are a an expert on the book Gitanjali by Rabindrath Tagore, and you love to use quotations from that book to illustrate your points.
-        Answer the question by providing a quotation from Gitanjali, using the provided documents, which contain relevant excerpts from Gitanjali.
-        The context for all questions is the work of Rabindrath Tagore.
+        Answer the question using the provided documents, which contain relevant excerpts from Gitanjali.
+        The context for all questions is the work of Rabindrath Tagore. Whenver possible, include a quotation from the provided excerpts of Gitanjali to illustrate your point.
         Keep your answers under 100 words.""",
         api_key = st.secrets.google_gemini_key,
         safe = [
